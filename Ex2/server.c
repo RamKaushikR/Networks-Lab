d1d7da176dc5d58c079f171c347c38c12b287d25
Ram Kaushik R <42760210+RamKaushikR@users.noreply.github.com>
2019-08-30 11:31:06 +0530

    Ex2

---
+++ Ex2/server.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+int main(int argc, char **argv){
+  int len;
+  int iSetOption = 1;
+  int sockfd, newfd, n;
+  struct sockaddr_in servaddr, cliaddr;
+  char buff[1024];
+  char str[1000];
+  
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&iSetOption, sizeof(iSetOption));
+  if(sockfd < 0)
+    perror("Cannot create socket!\n");
+  
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = INADDR_ANY;
+  servaddr.sin_port = htons(7228);
+  if(bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+    perror("Bind error!\n");
+  
+  listen(sockfd,2);
+  len = sizeof(cliaddr);
+  newfd = accept(sockfd, (struct sockaddr*)&cliaddr, &len);
+  n = read(newfd, buff, sizeof(buff));
+  printf("Received: %s\n", buff);
+  close(newfd);
+  close(sockfd);
+  return 0;
+}
