ffbe166645120f6226b2fdf10e3bb3c8f8ff4c53
Ram Kaushik R <42760210+RamKaushikR@users.noreply.github.com>
2019-08-30 11:32:34 +0530

    Ex7

---
+++ Ex7/client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define MAXLINE 1024
+
+int main() {
+  int sockfd;
+  char buffer[MAXLINE];
+  char *dname = (char *) malloc (30);
+  struct sockaddr_in servaddr;
+
+  if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
+  memset(&servaddr, 0, sizeof(servaddr));
+
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(PORT);
+  servaddr.sin_addr.s_addr = INADDR_ANY;
+
+  int n, len;
+
+  printf("Enter the domain name: ");
+  scanf("%s",dname);
+  
+  sendto(sockfd, (const char *)dname, strlen(dname), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+  printf("Message sent\nURL: %s\n", dname);
+
+  n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+  buffer[n] = '\0';
+  
+  printf("Server : %s\n", buffer);
+  close(sockfd);
+  return 0;
+}
