d8eb794ad1b226795600ee4d045e4349c64eae15
RamKaushikR <ramkaushik17125@cse.ssn.edu.in>
2019-10-05 13:17:28 +0530

    Ex7

--- Ex7/client.c
+++ Ex7/client.c
@@ -1,43 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-
-#define PORT 8080
-#define MAXLINE 1024
-
-int main() {
-  int sockfd;
-  char buffer[MAXLINE];
-  char *dname = (char *) malloc (30);
-  struct sockaddr_in servaddr;
-
-  if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("socket creation failed");
-    exit(EXIT_FAILURE);
-  }
-  memset(&servaddr, 0, sizeof(servaddr));
-
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(PORT);
-  servaddr.sin_addr.s_addr = INADDR_ANY;
-
-  int n, len;
-
-  printf("Enter the domain name: ");
-  scanf("%s",dname);
-  
-  sendto(sockfd, (const char *)dname, strlen(dname), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-  printf("Message sent\nURL: %s\n", dname);
-
-  n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-  buffer[n] = '\0';
-  
-  printf("Server : %s\n", buffer);
-  close(sockfd);
-  return 0;
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<string.h>
+int main(int argc, char **argv)
+{
+    unsigned int len;
+    int sockfd, n = 0;
+    struct sockaddr_in servaddr, cliaddr;
+    char buff[100];
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0)
+        perror("Cannot create socket!\n");
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+    servaddr.sin_port = htons(7228);
+    len = sizeof(servaddr);
+    while(1) {
+        printf("Domain Name: ");
+        scanf("%s", buff);
+        n = sendto(sockfd, buff, sizeof(buff), MSG_DONTWAIT, (struct sockaddr*)&servaddr, sizeof(servaddr));
+        n = recvfrom(sockfd, buff, sizeof(buff), MSG_WAITALL, (struct sockaddr*)&servaddr, &len);
+        printf("The IP address is %s\n", buff);
+    }
+    close(sockfd);
+    return 0;
 }
--- Ex7/server.c
+++ Ex7/server.c
@@ -1,52 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <netdb.h>
-
-#define PORT 8080
-#define MAXLINE 1024
-
-int main() {
-  int sockfd;
-  char buffer[MAXLINE];
-  char *ip = "IP Address not found";
-  struct sockaddr_in servaddr, cliaddr;
-  struct hostent *h;
-
-  if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("socket creation failed\n");
-    exit(EXIT_FAILURE);
-  }
-  
-  memset(&servaddr, 0, sizeof(servaddr));
-  memset(&cliaddr, 0, sizeof(cliaddr));
- 
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = INADDR_ANY;
-  servaddr.sin_port = htons(PORT);
-
-  if(bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
- 
-  int len, n;
-  n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
-  buffer[n] = '\0';
-  printf("Client : %s\n", buffer);
-  
-  h = gethostbyname(buffer);
-  if(h)
-    ip = inet_ntoa(*((struct in_addr *)h->h_addr));
-
-  printf("%s\n", ip);
-  sendto(sockfd, (const char *)ip, strlen(ip), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-  sleep(2);
-  return 0;
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+typedef struct node Node;
+struct node
+{
+    char domain[100];
+    char ip[16];
+    Node *next;
+};
+Node *getNode()
+{
+    return (Node *)malloc(sizeof(Node));
+}
+Node *addNode(Node* head, char domain[], char ip[])
+{
+    Node *temp = NULL;
+    if(head==NULL) {
+        head = getNode();
+        strcpy(head->domain, domain);
+        strcpy(head->ip, ip);
+        head->next = NULL;
+    }
+    else {
+        for(temp = head; temp->next != NULL; temp = temp->next);
+        Node *n = getNode();
+        strcpy(n->domain, domain);
+        strcpy(n->ip, ip);
+        n->next = NULL;
+        temp->next = n;
+    }
+    return head;
+}
+int main(int argc, char **argv)
+{
+    unsigned int len, flag;
+    int sockfd, newfd, n = 0;
+    struct sockaddr_in servaddr, cliaddr;
+    char buff[100];
+    struct hostent *he;
+    struct in_addr **addr_list;
+    Node *table = NULL, *temp = NULL;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0)
+        perror("Cannot create socket!\n");
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(7228);
+    if(bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+        perror("Bind error!\n");
+    len = sizeof(cliaddr);
+    while(1) {
+        flag = 0;
+        n = recvfrom(sockfd, buff, sizeof(buff), MSG_WAITALL, (struct sockaddr*)&cliaddr, &len);
+        if(n != 0) {
+            printf("DNS request for ");
+            printf("%s\n", buff);
+            for(temp = table; temp != NULL && flag == 0; temp = temp->next) {
+                if(strcmp(buff, temp->domain) == 0) {
+                    flag = 1;
+                    strcpy(buff, temp->ip);
+                    printf("Present in table: %s\n", buff);
+                }
+            }
+            if(flag == 0) {
+                he = gethostbyname(buff);
+                addr_list = (struct in_addr **) he->h_addr_list;
+                table = addNode(table, buff, inet_ntoa(*addr_list[0]));
+                strcpy(buff, inet_ntoa(*addr_list[0]));
+                printf("Added to table: %s\n", buff);
+            }
+            n = sendto(sockfd, buff, sizeof(buff), MSG_WAITALL, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+        }
+    }
+    close(sockfd);
+    return 0;
 }
